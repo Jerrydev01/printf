aa9c45ae1a56fb3dcf9151c5b7d2d70d4172444a
17Ola <hollawunmi@gmail.com>
2022-10-18 05:39:02 -0700

    helper funtion

--- _helper_function.c
+++ _helper_function.c
@@ -20,3 +20,40 @@ int print_binary(int num)
 	len++;
 	return (len);
 }
+/**
+ *format_spec - format specifier.
+ * @str: format string
+ * @args: argument list
+ * @i: current loop point
+ *
+ * Return: Nothing
+ */
+
+int format_spec(const char *str, va_list args, int i)
+{
+	char *string;
+	int len = 0;
+
+	switch (str[i])
+	{
+		case 'c':
+			print_char(va_arg(args, int));
+			return (1);
+		case 's':
+			string = va_arg(args, char *);
+			len += print_str(string);
+			return (len);
+		case '%':
+			print_char('%');
+			return (1);
+                case 'd':len += print_num(va_arg(args, int));
+			return (len);
+		case 'i':
+			len += print_num(va_arg(args, int));
+			return (len);
+		case 'b':
+			len += print_binary(va_arg(args, int));
+			return (len);
+	}
+	return (0);
+}
